2cc7da58f66bd8bcb58b506099941f53f416d059
Neto.Changes <infero-sky@yandex.ru>
2023-01-19 02:35:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketAmount = 10_000;
+        int bonus = 20;
+
+        System.out.println("Количество бонусов: " + ticketAmount / bonus);
+    }
+}
