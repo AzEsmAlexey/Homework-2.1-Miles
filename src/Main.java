81fe5a28c57ec54a764b90aa0277307fde88fad9
Neto.Changes <infero-sky@yandex.ru>
2023-01-24 17:09:33 +0300

    modification3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketAmount = 10_000;
-        int bonus = 20;
-
-        System.out.println("Количество бонусов: " + ticketAmount / bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
